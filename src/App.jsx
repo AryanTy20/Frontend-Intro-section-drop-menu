2e0f90b5b857f4a0624e915e0ee66296619277d8
AryanTy20 <devty007@gmail.com>
2022-09-14 18:26:38 +0530

    credit

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,14 @@ const App = () => {
       <div className="app">
         <Header />
         <Main />
+
+        <div className="attribution">
+          Challenge by
+          <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+            Frontend Mentor
+          </a>
+          . Coded by <a href="https://github.com/AryanTy20">Aryan Tirkey</a>.
+        </div>
       </div>
     </>
   );
